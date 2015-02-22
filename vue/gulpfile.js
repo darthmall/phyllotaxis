9af9a8bd43374c3d7792a1d41339de616a84f110
W. Evan Sheehan <wallace.sheehan@gmail.com>
2015-02-21 19:10:43 -0500

    Add a path object to the gulpfile
    Add an object to define all the paths and file globs used in the gulp tasks at
    the top of the file. This helps for documentation and to ease updating of
    paths. Some of these paths presage things like migrating to LESS.

--- vue/gulpfile.js
+++ vue/gulpfile.js
@@ -8,30 +8,41 @@ var gulp        = require('gulp');
 var serveStatic = require('serve-static');
 var source      = require('vinyl-source-stream');
 
+var path = {
+  'build'   : 'build/',
+  'dist'    : 'dist/',
+  'fonts'   : 'fonts/**/*.{ttf,otf,svg,eot,woff}',
+  'html'    : 'index.html',
+  'main'    : 'js/Phyllotaxis.js',
+  'scripts' : 'js/**/*.js',
+  'styles'  : 'styles/**/*.less',
+  'tests'   : 'tests/'
+};
+
 // load plugins
 var $ = require('gulp-load-plugins')();
 
-function build(src, dst, opts) {
-  var bundleStream = browserify(src, opts).bundle()
+function build(opts) {
+  var bundleStream = browserify(path.main, opts).bundle()
     .on('error', function (e) {
       $.util.log(e.message);
       this.emit('end');
     });
 
   return bundleStream
-    .pipe(source(src))
+    .pipe(source(path.main))
     .pipe($.rename('Phyllotaxis.js'))
-    .pipe(gulp.dest(dst));
+    .pipe(gulp.dest(path.build));
 };
 
-gulp.task('scripts', function () {
-  return gulp.src('js/**/*.js')
+gulp.task('jshint', function () {
+  return gulp.src(path.scripts)
     .pipe($.jshint())
     .pipe($.jshint.reporter(require('jshint-stylish')));
 });
 
-gulp.task('browserify', ['scripts'], function () {
-  return build('./js/Phyllotaxis.js', './build/js', {
+gulp.task('browserify', ['jshint'], function () {
+  return build({
     debug     : true,
     standalone: 'Phyllotaxis',
     paths     : ['./js']
@@ -39,24 +50,28 @@ gulp.task('browserify', ['scripts'], function () {
 });
 
 gulp.task('fonts', function () {
-  return gulp.src('fonts/**/*')
-    .pipe(gulp.dest('./build/fonts'));
+  return gulp.src(path.fonts)
+    .pipe($.flatten())
+    .pipe(gulp.dest(path.build + 'fonts/'));
 });
 
 gulp.task('styles', function () {
-  return gulp.src('styles/**/*.css')
-    .pipe(gulp.dest('./build/styles'));
+  return gulp.src(path.styles)
+    .pipe(gulp.dest(path.build));
 });
 
 gulp.task('html', function () {
-  return gulp.src(['**/*.html', '!node_modules/**/*'])
-    .pipe(gulp.dest('./build'));
+  return gulp.src(path.html)
+    .pipe(gulp.dest(path.build));
 });
 
 gulp.task('build', ['browserify', 'styles', 'fonts', 'html']);
 
+gulp.task('dist', ['styles', 'fonts', 'html'], function () {
+});
+
 gulp.task('clean', function (cb) {
-  del('build', cb);
+  del([path.build, path.dist], cb);
 });
 
 gulp.task('watch', ['build'], function () {
